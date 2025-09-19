f3be9db0f7eed21a1cc016bd7aa0f85d411b0cc3
Nicolò Ginatta <n.ginatta@fusionailabs.it>
2025-09-19 14:50:18 +0200

    Implemented SFT

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from post-training!")
+import sft
 
+def main():
+    sft.run(
+        model_name="Qwen/Qwen3-0.6B-Base",
+        dataset_path="banghua/DL-SFT-Dataset",
+        questions = [
+            "Give me an 1-sentence introduction of LLM.",
+            "Calculate 1+1-1",
+            "What's the difference between thread and process?"
+        ],
+        USE_GPU=False
+    )
 
 if __name__ == "__main__":
     main()
---
+++ sft.py
@@ -0,0 +1,92 @@
+import torch
+from datasets import load_dataset
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from trl import SFTConfig, SFTTrainer
+
+def generate_response(model, tokenizer, user_message, system_message=None, max_token=100):
+    messages = []
+    if system_message:
+        messages.append({"role": "system", "content": system_message})
+
+    messages.append({"role": "user", "content": user_message})
+
+    prompt = tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True,
+        enable_thinking=False,
+    )
+
+    inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+
+    with torch.no_grad():
+        generation_output = model.generate(
+            **inputs,
+            max_new_tokens=max_token,
+            do_sample=False,
+            pad_token_id=tokenizer.eos_token_id,
+            eos_token_id=tokenizer.eos_token_id
+    )
+        
+    input_length = inputs.input_ids.shape[1]
+    generated_text = tokenizer.decode(generation_output[0][input_length:], skip_special_tokens=True)
+    return generated_text
+
+def test_model_with_questions(model, tokenizer, questions, 
+                              system_message=None, title="Model Output"):
+    print(f"\n=== {title} ===")
+    for i, question in enumerate(questions, 1):
+        response = generate_response(model, tokenizer, question, 
+                                      system_message)
+        print(f"\nModel Input {i}:\n{question}\nModel Output {i}:\n{response}\n")
+
+def load_model_and_tokenizer(model_name, USE_GPU=False):
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name)
+
+    if USE_GPU:
+        model = model.to("cuda")
+
+    if not tokenizer.chat_template:
+        tokenizer.chat_template = """{% for message in messages %}
+                {% if message['role'] == 'system' %}System: {{ message['content'] }}\n
+                {% elif message['role'] == 'user' %}User: {{ message['content'] }}\n
+                {% elif message['role'] == 'assistant' %}Assistant: {{ message['content'] }} <|endoftext|>
+                {% endif %}
+                {% endfor %}"""
+    
+    if not tokenizer.pad_token:
+        tokenizer.pad_token = tokenizer.eos_token
+
+    return model, tokenizer
+
+def run(model_name, dataset_path, questions, USE_GPU):
+    model, tokenizer = load_model_and_tokenizer(model_name, USE_GPU)
+
+    train_dataset = load_dataset(dataset_path)['train']
+    if not USE_GPU:
+        train_dataset=train_dataset.select(range(150))
+
+    sft_config = SFTConfig(
+        learning_rate=8e-5,
+        num_train_epochs=1,
+        per_device_train_batch_size=1,
+        gradient_accumulation_steps=8,
+        gradient_checkpointing=False,
+        logging_steps=2
+    )
+
+    sft_trainer = SFTTrainer(
+        model=model,
+        args=sft_config,
+        train_dataset=train_dataset,
+        processing_class=tokenizer
+    )
+
+    sft_trainer.train()
+
+    if not USE_GPU: # move model to CPU when GPU isn’t requested
+        sft_trainer.model.to("cpu")
+    
+    test_model_with_questions(sft_trainer.model, tokenizer, questions, 
+                          title="Base Model (After SFT) Output")
